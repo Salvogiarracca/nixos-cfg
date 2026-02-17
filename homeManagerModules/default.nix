218e0c6ed25ad1c7208a90366ecebbc214e39311
Salvatore Giarracca <salvogiarracca07@gmail.com>
2026-02-17 17:39:32 +0100

    feat: distinction between cli and gui programs
    - should wezterm be considered gui or cli?

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -1,15 +1,20 @@
 {lib, ...}: {
   imports = [
+    #CLI
     ./cliPrograms/nvf.nix
-    ./cliPrograms/wezterm.nix
-    ./cliPrograms/vscode.nix
     ./cliPrograms/git.nix
     ./cliPrograms/shell.nix
+    #GUI
+    ./guiPrograms/wezterm.nix
+    ./guiPrograms/vscode.nix
   ];
 
+  #CLI
   nvf.enable = lib.mkDefault true;
-  wezterm.enable = lib.mkDefault true;
-  vscode.enable = lib.mkDefault true;
   git.enable = lib.mkDefault true;
   shell.enable = lib.mkDefault true;
+
+  #GUI
+  wezterm.enable = lib.mkDefault true;
+  vscode.enable = lib.mkDefault true;
 }
