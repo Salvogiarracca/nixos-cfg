82b8c6f8d9a28ae1249ae7b7c0260009a00b9f2d
Salvatore Giarracca <salvogiarracca07@gmail.com>
2026-02-19 11:18:14 +0100

    feat: yazi plugins

--- homeManagerModules/cliPrograms/yazi.nix
+++ homeManagerModules/cliPrograms/yazi.nix
@@ -1,8 +1,16 @@
 {
+  pkgs,
   lib,
   config,
   ...
-}: {
+}: let
+  yazi-plugins = pkgs.fetchFromGitHub {
+    owner = "yazi-rs";
+    repo = "plugins";
+    rev = "25918dcde97f11ac37f80620cc264680aedc4df8";
+    hash = "sha256-TzHJNIFZjUOImZ4dRC0hnB4xsDZCOuEjfXRi2ZXr8QE=";
+  };
+in {
   options = {
     yazi.enable = lib.mkEnableOption "enable yazi";
   };
@@ -16,6 +24,17 @@
           mgr = {
             show_hidden = true;
             ratio = [2 2 4];
+          };
+        };
+        plugins = {
+          chmod = "${yazi-plugins}/chmod.yazi";
+          full-border = "${yazi-plugins}/full-border.yazi";
+          toggle-pane = "${yazi-plugins}/toggle-pane.yazi";
+          fr = pkgs.fetchFromGitHub {
+            owner = "lpng";
+            repo = "fr.yazi";
+            rev = "aa88cd4d4345c07345275291c1a236343f834c86";
+            sha256 = "sha256-3D1mIQpEDik0ppPQo+/NIhCxEu/XEnJMJ0HiAFxlOE4=";
           };
         };
       };
